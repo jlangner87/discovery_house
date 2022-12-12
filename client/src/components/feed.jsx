d3bd74e5987807c62dd94cdfd37b09b693e98c4a
jlangner87 <josh.langner@gmail.com>
2022-12-12 14:43:18 -0600

    style event feed for mobile

--- client/src/components/feed.jsx
+++ client/src/components/feed.jsx
@@ -6,7 +6,17 @@ function Feed() {
           <h1 className="feed_header">What's new?</h1>
         </div>
         <div className="feed_card">
-          <h3 className="feed_title">Event Name</h3>
+          <h3 className="feed_title">Update Title</h3>
+          <p className="feed_detail">Event Date and Time</p>
+          <p className="feed_content">Donec ac odio tempor orci dapibus ultrices in. Egestas quis ipsum suspendisse ultrices. Maecenas pharetra convallis posuere morbi. Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Aenean pharetra magna ac placerat vestibulum. At in tellus integer feugiat scelerisque varius. Aliquet nec ullamcorper sit amet risus nullam eget felis. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sapien faucibus et molestie ac.</p>
+        </div>
+        <div className="feed_card">
+          <h3 className="feed_title">Update Title</h3>
+          <p className="feed_detail">Event Date and Time</p>
+          <p className="feed_content">Donec ac odio tempor orci dapibus ultrices in. Egestas quis ipsum suspendisse ultrices. Maecenas pharetra convallis posuere morbi. Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Aenean pharetra magna ac placerat vestibulum. At in tellus integer feugiat scelerisque varius. Aliquet nec ullamcorper sit amet risus nullam eget felis. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sapien faucibus et molestie ac.</p>
+        </div>
+        <div className="feed_card">
+          <h3 className="feed_title">Update Title</h3>
           <p className="feed_detail">Event Date and Time</p>
           <p className="feed_content">Donec ac odio tempor orci dapibus ultrices in. Egestas quis ipsum suspendisse ultrices. Maecenas pharetra convallis posuere morbi. Fermentum posuere urna nec tincidunt praesent semper feugiat nibh. Aenean pharetra magna ac placerat vestibulum. At in tellus integer feugiat scelerisque varius. Aliquet nec ullamcorper sit amet risus nullam eget felis. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Sapien faucibus et molestie ac.</p>
         </div>
