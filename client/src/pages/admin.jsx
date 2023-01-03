1a0892a61ff565e5e7e743dcd1477b242217d36b
jlangner87 <josh.langner@gmail.com>
2023-01-03 14:03:07 -0600

    reconfigure protected route for auth setup

--- client/src/pages/admin.jsx
+++ client/src/pages/admin.jsx
@@ -1,12 +1,18 @@
 import HomeLink from '../components/homeLink'
 import LoginButton from '../components/LoginButton'
+import { NavLink } from 'react-router-dom'
 
 function Admin() {
+
   return (
     <div className="page">
       <HomeLink/>
-      <h1 className='page_title'>Please log in to access this page.</h1>
+      <h3 className='page_title'>Admin Page</h3>
+      <h1 className='page_content_subheader'>You must be logged in to access this page.</h1>
       <LoginButton/>
+      <h2 className='page_content_subheader'>-OR-</h2>
+      <NavLink to='/'><button className='login_button'>Go Back</button></NavLink>
+
     </div>
   )
 }
--- client/src/pages/crud.jsx
+++ client/src/pages/crud.jsx
@@ -5,7 +5,7 @@ function CRUD() {
   return (
     <div className="page">
       <HomeLink/>
-      <h3 className='page_title'>The CURUD page</h3>
+      <h3 className='page_title'>Admin Page</h3>
       <p>This is where I will import the forms for creating, updating and deleting events and announcements</p>
       <LogoutButton/>
     </div>
