0e84101f3bb35ec1375f54cc1f06d5fe757df0a3
jlangner87 <josh.langner@gmail.com>
2023-05-31 09:25:34 -0500

    add create post form

--- client/src/pages/crud.jsx
+++ client/src/pages/crud.jsx
@@ -4,7 +4,20 @@ function CRUD() {
   return (
     <div className="page">
       <h3 className='page_title'>Admin Page</h3>
-      <p>This is where I will import the forms for creating, updating and deleting events and announcements</p>
+      <div>
+        <h3>Create a new post</h3>
+        <div className='post_form'>
+          <label htmlFor="">Title</label>
+          <input id='title' type="text" />
+          <label htmlFor="content">Content</label>
+          <textarea id="content" type="text" />
+          <label htmlFor="image">Image (optional)</label>
+          <input type="url" />
+          <label htmlFor="link">Link (optional)</label>
+          <input type="url" />
+          <button>submit</button>
+        </div>
+      </div>
       <LogoutButton/>
     </div>
   )
